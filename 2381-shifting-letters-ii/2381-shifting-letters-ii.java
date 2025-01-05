9ad84889f72b8f8dbb3c618a86a9411f319ddb6a
jerry <alokkumar.jer@gmail.com>
2025-01-05 10:14:59 +0530

    Time: 7 ms (74.53%), Space: 73.8 MB (90.57%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -2,12 +2,11 @@ class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
         int n = s.length();
         int[] diff = new int[n + 1]; 
-        
         for (int[] shift : shifts) {
             int start = shift[0];
             int end = shift[1];
             int direction = shift[2];
-            
+
             if (direction == 1) { 
                 diff[start] += 1;
                 diff[end + 1] -= 1;
@@ -20,15 +19,17 @@ class Solution {
         int cumulative = 0;
         for (int i = 0; i < n; i++) {
             cumulative += diff[i];
-            netShifts[i] = cumulative;
+            netShifts[i] = cumulative % 26; 
         }
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
             char originalChar = s.charAt(i);
-            int shift = netShifts[i];
-            char newChar = (char) ((originalChar - 'a' + shift + 26) % 26 + 'a');
+            int shift = (netShifts[i] + 26) % 26;
+            char newChar = (char) ((originalChar - 'a' + shift) % 26 + 'a');
             result.append(newChar);
         }
+
         return result.toString();
+    
     }
 }
